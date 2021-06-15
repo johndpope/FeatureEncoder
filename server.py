c44e0ff7c0a71664c8493e41ea40fab6ca573023
acm890 <saifaldeen.alkhateeb@haw-hamburg.de>
2021-06-15 13:39:55 +0000

    update server file

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request, abort
 
+import os
 import numbers
+import json
+import base64
+
 
 from fee import run
 
@@ -8,19 +12,35 @@ webservice = Flask(__name__)
 
 @webservice.route('/featureencoder', methods=['POST'])
 def create_new_employee():
+    print(request)
     if not request.json or not 'image' in request.json:
         abort(400, "Post JSON with property image")
     
     # 
-    input_image = request.json['image']
-
-    # Validationdd
-    if not isinstance(input_image, numbers.Number) :
-        abort(400, "image should be a number")
+    image_decoded = request.json['image']
+    input_image = base64.b64decode(image_decoded)
+    if not os.path.exists("temp"):
+        os.mkdir("temp")
     
-    latentspace = run(input_image)
+    image_path = "temp/image.png"
+    with open(image_path , 'wb') as file:
+        file.write(input_image)
+        
+    # Validationdd
+    #if not isinstance(input_image, numbers.Number) :
+    #    abort(400, "image should be a number")
+    latentspace = run(image_path)
+    #print(latent_space.shape)
+    #["0": [18, 512]]
+    #for item in latentspace.items():
+    #    print("Key", item[0])
+    #    print("values  shape: ", item[1])
+        
+    latentspace_list = latentspace[0][0][len(latentspace[0])-1].tolist()
 
-    return jsonify({'latentspace': latentspace}), 200
+    latentspace_list_str = json.dumps(latentspace_list)
+    
+    return jsonify({'latentspace': latentspace_list_str}), 200
 
 if __name__ == '__main__':
     webservice.run(debug=False, use_reloader=False)
